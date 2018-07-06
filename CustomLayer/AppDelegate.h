37ac7210a8e9ac7a6e2b39b5183596fc8008401c
乐培培 <peyton@yuepeipeidemacbook-pro.local>
2018-07-06 22:54:36 +0800

    Initial Commit

---
+++ CustomLayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomLayer
+//
+//  Created by 乐培培 on 2018/7/6.
+//  Copyright © 2018年 乐培培. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CustomLayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomLayer
+//
+//  Created by 乐培培 on 2018/7/6.
+//  Copyright © 2018年 乐培培. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
